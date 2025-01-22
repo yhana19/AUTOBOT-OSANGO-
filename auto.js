2ef596bb56d094b5f8be26432367eca9d72e61af
さ METOUSHELA. さ <168326500+MeganAe@users.noreply.github.com>
2025-01-22 21:43:32 +0200

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const login = require('./fb-chat-api/index');
+const login = require('./metoushela/index');
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
